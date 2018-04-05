2c6a000dde4e43ae4fff4216d8c7dfdb1d9c2c68
Adrian <Rodriguez.Adrian609@gmail.com>
2018-04-05 19:31:47 -0400

    Added frocert list and classes for send and recieving list

--- src/GroceryItem.js
+++ src/GroceryItem.js
@@ -1,1 +1,15 @@
 // your GroceryItem code, here
+/** 
+* Create a `GroceryItem` class within the `src/GroceryItem.js` file.
+* A new `GroceryItem` should take three arguments, the name of the item, the price of the item, and the quantity.
+*/
+class GroceryItem{
+    constructor(name, price, quantity){
+        this.name= name;
+        this.price=" Price: $ "+price.toFixed(2);
+        this.quantity=" Quantity: # "+quantity;
+    }
+}
+
+
+
--- src/main.js
+++ src/main.js
@@ -1,9 +1,46 @@
 // Meets Expectations
+document.getElementById("getList").addEventListener('click', onClick);
+document.getElementById("deleteItem").addEventListener('click', deleteItem);
 
-
-
-
-
+// In `main.js` write code to create four different `
+// GroceryItems` and store those `GroceryItems` objects in an **array** called `groceryList`.
+var groceryList = [
+    new GroceryItem("apple", .50, 5),
+    new GroceryItem("Banana", .70, 2),
+    new GroceryItem("Pizza", 2.50, 1),
+    new GroceryItem("Corn meal", 1.50, 1)
+  ];
+// In `main.js` create a `summary` function that takes 1 argument, an array of `GroceryItems`. 
+// When you call `summary(groceryList);` your function should 
+function onClickSelectedItem(){
+  document.getSelection().addEventListener('click',selectedItem)
+}
+function onClick(){
+  summary(groceryList);
+}
+function selectedItem() {
+  var x = document.getSelection("");
+  alert(x);
+  console.log(x)
+}
+function summary(arr) {
+  var output = `<p><pre>Inventory:</pre></p><ul id="mySelect">`;
+  var objCopy={arr};
+  console.log(objCopy);
+  for(var i =0;i<arr.length;i++) {
+        var obj= arr[i];
+        output += `<li data-toggle="list" class="list-group-item deleteItem">${i+1+" - "+Object.values(obj)}</li>`;
+        console.log(arr[i]);
+  } 
+  output +=`<ul>`;
+  console.log(output);
+  document.getElementById("main").innerHTML = output;
+}
+function deleteItem(summaryList){
+    
+  
+  summaryList.splice();
+}
 
 // Exceeds Expectations
 let inventoryNearMe = {
@@ -31,3 +68,4 @@ let inventoryNearBestie = {
   avocado: 10,
   milk: 5
 };
+
